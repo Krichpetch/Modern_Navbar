fae47832b002d78996461c1292c7d52ce6f02187
Krichpetch <s6507012662021@email.kmutnb.ac.th>
2024-09-09 15:42:26 +0700

    first commit

---
+++ lib/constant/constant.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const themePrimary = Color.fromARGB(255, 19, 49, 141);
+const themeSecondary = Color.fromARGB(255, 29, 27, 61);
+
+const highlightPrimary = Color.fromARGB(255, 255, 214, 33);
+const highlightSecondary = Color.fromARGB(255, 255, 242, 97);
+const highlightTertiary = Color.fromARGB(255, 198, 237, 242);
+
+const specialPrimary = Color.fromARGB(255, 111, 122, 222);
+
+final textSubTitle = GoogleFonts.spaceGrotesk( fontSize: 18, color: Color.fromARGB(255, 198, 237, 242), fontWeight: FontWeight.w400,);
+final textTitle = GoogleFonts.spaceGrotesk( fontSize: 24, color: highlightSecondary, fontWeight: FontWeight.w700);
+final textButton = GoogleFonts.spaceGrotesk( fontSize: 24, color: highlightSecondary, fontWeight: FontWeight.w400);
+final textMenu = GoogleFonts.spaceGrotesk( fontSize: 18, color: highlightTertiary, fontWeight: FontWeight.w300);
+// <palette>
+//   <color name="Air superiority blue" hex="78A4C1" r="120" g="164" b="193" />
+//   <color name="Light cyan" hex="C6EDF2" r="198" g="237" b="242" />
+//   <color name="YInMn Blue" hex="354D72" r="53" g="77" b="114" />
+//   <color name="Space cadet" hex="1D1B3D" r="29" g="27" b="61" />
+//   <color name="Egyptian blue" hex="13318D" r="19" g="49" b="141" />
+//   <color name="Royal blue (traditional)" hex="142B78" r="20" g="43" b="120" />
+// </palette>
